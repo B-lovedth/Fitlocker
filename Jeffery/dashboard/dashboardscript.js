96e2f8d5eff959a6628ea4ba88ba6cd61653cd15
Young Jeffery <mayowaidowu111@gmail.com>
2025-03-09 23:29:00 +0100

    my work so far
    Done with the Dashboard
    Edits will be done on the Homepage
    
    No mobile UI done yet
    Also note that linked files and images would need to be updated when moving files out of their folders

---
+++ Jeffery/dashboard/dashboardscript.js
@@ -0,0 +1,13 @@
+function clearSearch() {
+  document.getElementById("searchBox").value = "";
+}
+
+function switchActive(clickedButton) {
+  const buttons = document.querySelectorAll(".btn");
+
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  clickedButton.classList.add("active");
+}
---
+++ Jeffery/homepage/homepagescript.js
@@ -0,0 +1,7 @@
+document.getElementById("nav-button").addEventListener("click", function () {
+  window.location.href = "homepage.html";
+});
+
+document.getElementById("main-button").addEventListener("click", function () {
+  window.location.href = "homepage.html";
+});
